b2ce54353f08231dee7be991ea0c39ad739c069e
sushanthk15 <k.sushanth.k@gmail.com>
2019-07-01 22:52:48 +0200

    Added an additional condition to check when the sigma_equivalent is zero. During this case, the C matrix is equivalent to the one in elastic Case(Hard-Coded) . The K parameter was corrected in this commit as well.

--- materialroutine30June.py
+++ materialroutine30June.py
@@ -1,26 +1,31 @@
 def material_routine(epsilon, epsilon_plastic_old, lamda, mu , sigmaY, youngs_modulus):
-	C_ijkl = (2*mu+lamda)*np.eye(3) #np.eye(3) = 3x3 Identity Matrix
-	sigma_trial = np.dot(C_ijkl,(epsilon - epsilon_plastic_old)) #3x1 Matrix
-	sigma_trial_dev = sigma_trial - np.sum(sigma_trial)/3
-	sigma_trial_eq = np.sqrt((1.5*np.dot((np.transpose(sigma_trial_dev)),sigma_trial_dev))) #Scalar
-	
-	
-	
-	#Yield Condition ------> Check for Elastic and Plastic Case 
-	if (sigma_trial_eq - sigmaY) < 0:
-		print("This is in Elastic Region")
-		#C_t = np.array([(2*mu + lamda), lamda, lamda,lamda,(2*mu + lamda), lamda, lamda,lamda,(2*mu + lamda)]).reshape(3,3)
-		plastic_multiplier = 0
-		#sigma_3d = sigma_trial
-	else :
-		print("This is in  Plastic Region")
-		plastic_multiplier = sigma_trial_eq /(3*mu + sigmaY)
-	
-	# Stress, C_t and epsilon_plastic_old update
-	sigma_new = np.sum(sigma_trial)/3 + ((sigma_trial_eq - 3*mu*plastic_multiplier)/sigma_trial_eq)*sigma_trial_dev
-	
-	C_t = ((lamda - 3*mu)/3)*np.eye(3) + (2*mu*(sigma_trial_eq -3*mu*plastic_multiplier)/sigma_trial_eq)*(2/3*np.eye(3)) - 3*mu*np.dot(sigma_trial_dev, np.transpose(sigma_trial_dev))
-	
-	epsilon_plastic_new = epsilon_plastic_old + plastic_multiplier*np.sign(sigma_new)
-	
-	return sigma_new, C_t, epsilon_plastic_new
+    C_ijkl = (2*mu+lamda)*np.eye(3) #np.eye(3) = 3x3 Identity Matrix
+    sigma_trial = np.dot(C_ijkl,(epsilon - epsilon_plastic_old)) #3x1 Matrix
+    sigma_trial_dev = sigma_trial - np.sum(sigma_trial)/3
+    sigma_trial_eq = np.linalg.norm((1.5*np.dot((np.transpose(sigma_trial_dev)),sigma_trial_dev))) #Scalar
+    
+    if sigma_trial_eq ==0:
+		C_t1 = np.array([(2*mu + lamda), lamda, lamda,lamda,(2*mu + lamda), lamda, lamda,lamda,(2*mu + lamda)]).reshape(3,3)
+		sigma_new = 2*mu*(epsilon - epsilon_plastic_old) + lamda*np.sum(epsilon - epsilon_plastic_old)
+		epsilon_plastic_new = epsilon_plastic_old
+	else:
+		
+		#Yield Condition ------> Check for Elastic and Plastic Case 
+		if (sigma_trial_eq - sigmaY) < 0:
+			print("This is in Elastic Region")
+			#C_t1 = np.array([(2*mu + lamda), lamda, lamda,lamda,(2*mu + lamda), lamda, lamda,lamda,(2*mu + lamda)]).reshape(3,3)
+			plastic_multiplier = 0
+			#sigma_3d = sigma_trial
+		else :
+			print("This is in  Plastic Region")
+			plastic_multiplier = sigma_trial_eq /(3*mu + sigmaY)
+		
+		# Stress, C_t and epsilon_plastic_old update
+		
+		sigma_new = np.sum(sigma_trial)/3 + ((sigma_trial_eq - 3*mu*plastic_multiplier)/sigma_trial_eq)*sigma_trial_dev
+		
+		C_t = ((3*lamda + 2*mu)/3)*np.eye(3) + (2*mu*(sigma_trial_eq -3*mu*plastic_multiplier)/sigma_trial_eq)*(2/3*np.eye(3)) - 3*mu*np.dot(sigma_trial_dev, np.transpose(sigma_trial_dev))/(sigma_trial_eq)**2
+		#C_t1 = np.array([(2*mu + lamda), lamda, lamda,lamda,(2*mu + lamda), lamda, lamda,lamda,(2*mu + lamda)]).reshape(3,3)
+		epsilon_plastic_new = epsilon_plastic_old + plastic_multiplier*np.sign(sigma_new)
+    
+    return sigma_new, C_t, epsilon_plastic_new #,C_t1, C_ijkl
